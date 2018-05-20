d585d761a418b9019a03779094d4a576d91ed962
강준서 <karl2003@naver.com>
2018-05-20 21:24:13 +0900

    0520. 유전 알고리즘 2
    아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 변이 만들기 귀찮다ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,39 +1,22 @@
-import random
+# -*- coding: utf-8 -*-
 
-'''
-세대 : 염색체 집합, Generation
-염색체 : 유전자 집합, Chromosome
-유전자 : 정보 집합, Gene
-'''
+import random
 
 
 class GeneticMethod:
-    def __init__(self, generation):
+    def __init__(self, generation, cost):
         self.generation = generation
         self.length = len(generation)  # 세대에 속한 염색체 수
+        self.cost = cost
 
     def evolution(self, method):  # 진화 메서드
-        '''
-        인자 : 세대, (선택, 교차, 변이)
-        :return: 진화된 해집합        '''
-
         parents = self.select(method=method[0], generation=self.generation)  # 부모 유전자 선택
         offspring = self.cross(method=method[1], parents=parents)  # 자손 유전자 생성
-        #  result = self.mutant(method=method[2], target=offspring)  # 세대에 변이 적용
-        result = offspring
+        result = self.mutant(method=method[2], target=offspring)  # 세대에 변이 적용
 
         return result
 
-
-    def select(self, method, generation, cost, selective_pressure=0):  # 선택 메서드
-        '''
-        :param method: 선택 연산의 방식(0-토너먼트, 1-품질 비례 룰렛휠)
-        :param generation: 선택 연산을 적용할 세대
-        :param cost: 염색체 비용 함수
-        :param selective_pressure: 선택압
-        :return: 두 쌍의 부모 염색체
-        '''
-
+    def select(self, method, generation, selective_pressure=0.6):  # 선택 메서드
         # 토너먼트 선택
         if method is 0:
             result = []
@@ -41,16 +24,20 @@ class GeneticMethod:
             for i in range(self.length):
                 r = []
                 for _ in range(2):
-                    if random.random()>=selective_pressure:
-                        r.append(max(random.choice(generation), random.choice(generation)))
+                    if random.random() >= selective_pressure:
+                        rev = False
                     else:
-                        r.append(min(random.choice(generation), random.choice(generation)))
+                        rev = True
+                    r.append(
+                        sorted([random.choice(generation), random.choice(generation)], key=self.cost, reverse=rev)[0]
+                    )
+
                 result.append(r)
             return result
 
         # 품질 비례 룰렛휠 선택
-        if method is 1:  #  적합도 계산, 비례하여 룰렛휠 배분, 선택
-            costs = [cost(x) for x in generation]
+        if method is 1:  # 적합도 계산, 비례하여 룰렛휠 배분, 선택
+            costs = [self.cost(x) for x in generation]
             fitnesses = [min(costs)-cost + (min(costs)-max(costs))/(selective_pressure-1) for cost in costs]
             # 적합도 = (최소 가치-현재 가치)+ (최소 가치-최대 가치)/(선택압-1)
 
@@ -63,48 +50,47 @@ class GeneticMethod:
                     flow = 0
                     for fit in fitnesses:
                         flow += fit
-                        if dart<=flow:
+                        if dart <= flow:
                             r.append(generation[i])
                 result.append(r)
 
             return result
 
-
-    def cross(self, method, parents, selective_pressure=0, points = 0):  # 교차 메서드
-        '''
-        :param method: 교차 연산의 방식(0-균등 교차, 1-다점 교차, 2-PMX)
-        :param parents: 교차 연산을 적용할 부모 염색체 쌍의 집합
-        :param selective_pressure: 선택압(균등 교차)
-        :param points: 교차점(다점 교차)
-        :return:
-        '''
+    def cross(self, method, parents, selective_pressure=0.6):  # 교차 메서드
 
         # 균등 교차
         if method is 0:
             result = []
             for parent in parents:
                 r = []
-                for i in len(parent[0]):
+                for i in range(len(parent[0])):
                     if random.random() >= selective_pressure:
-                        r.append(max(parent[0][i], parent[1][i]))
+                        rev = False
                     else:
-                        r.append(min(parent[0][i], parent[1][i]))
+                        rev = True
+                    r.append(
+                        sorted(parent, key=self.cost, reverse=rev)[0][i]
+                    )
                 result.append(r)
             return result
 
-        # PMX
+        # 다점 교차
+        if method is 1:
+            pass
 
+        # PMX
+        if method is 2:
+            pass
 
     def mutant(self, method, target):  # 변이 메서드
-        pass
+        return 0
 
 
 def main():
     first_gen = []
     for i in range(100):
-        first_gen.append([random.choice(0, 1) for _ in range(6)])
+        first_gen.append([random.choice([0, 1]) for _ in range(6)])
 
-    generation = GeneticMethod(generation=first_gen)
     solution = [0, 1, 1, 1, 0, 1]
 
     def cost(chromo):
@@ -114,12 +100,13 @@ def main():
                 score -= 1
         return score
 
+    generation = GeneticMethod(generation=first_gen, cost=cost)
 
     for i in range(5000):
         generation.generation = generation.evolution(method=[0, 0])
 
     print(
-        sorted(generation.generation, key=cost, reverse=1)[0]
+        sorted(generation.generation, key=cost, reverse=True)[0]
     )
 
 
