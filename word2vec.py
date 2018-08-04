dbecfdd58fb3425cab1309923f59401d889ff5d0
강준서 <karl2003@naver.com>
2018-08-04 16:47:53 +0900

    0804. word2vec(2)

--- word2vec.py
+++ word2vec.py
@@ -1,31 +1,37 @@
 # Skip-Gram
 import tensorflow as tf
+import tensorflow.contrib as tfc
 
 
-def word2vec(batches, embedding_size, vocab_size):
+def word2vec(corpus, embedding_size, window):
+    vocabulary = []
+
+    for sentence in corpus:
+        sentence = sentence.split(' ')
+        vocabulary.append(sentence)
+
+    vocabulary = list(set(vocabulary))
+    vocab_size = len(vocabulary)
+
+    input_vec = tf.placeholder(tf.float32, [None, vocab_size])
+    output_vec = tf.placeholder(tf.float32, [1, vocab_size])
+
     embeddings = tf.Variable(
         tf.random_normal([vocab_size, embedding_size])
     )
+    embed = tf.nn.embedding_lookup(embeddings, input_vec)
 
     output_weights = tf.Variable(
-        tf.random_normal([embedding_size, vocab_size])/tf.sqrt(embedding_size)
+        tf.random_normal([embedding_size, vocab_size]) / tf.sqrt(embedding_size)
     )
     output_bias = tf.Variable(tf.random_normal([vocab_size]))
 
-    inputs = tf.placeholder(tf.float32, [None])
-    object_outputs = tf.placehoder(tf.float32, [None, 1])
-
-    embed = tf.nn.embedding_lookup(embeddings, inputs)
-
     loss = tf.reduce_mean(
-        tf.nn.nce_loss(output_weights, output_bias, object_outputs, embed, 5, vocab_size)
+        tf.nn.nce_loss(output_weights, output_bias, output_vec, embed, 5, vocab_size)
     )
-
     optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
-
     init = tf.global_variables_initializer()
 
     with tf.Session() as sess:
         sess.run(init)
-        for batch in batches:
-            sess.run(optimizer, feed_dict={inputs: batch[0], object_outputs: batch[1]})
+        # 문장에서 배치 뽑아내서 학습
